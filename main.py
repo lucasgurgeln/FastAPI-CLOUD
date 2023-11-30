2bd3279e160b15945523c1c3f2c9b31fd0254899
lucasgurgeln <lucasgurgel09@hotmail.com>
2023-11-30 15:42:19 -0300

    requirements

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
 from fastapi import FastAPI, HTTPException
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
 
 app = FastAPI()
 
-# Conexão com o banco de dados
+# Conexão com o banco de dados e definições de ORM
 DATABASE_URL = os.environ.get("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+SessionLocal = sessionmaker(bind=engine)
+
+# Modelo ORM para os items
+class Item(Base):
+    __tablename__ = 'items'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), index=True)
+
+# Criação das tabelas no banco de dados
+Base.metadata.create_all(bind=engine)
 
 @app.post("/items/")
 def create_item(name: str):
-    with engine.connect() as conn:
-        result = conn.execute(text("INSERT INTO items (name) VALUES (:name)"), {"name": name})
-        conn.commit()
-    return {"name": name}
+    db = SessionLocal()
+    new_item = Item(name=name)
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT * FROM items WHERE id = :item_id"), {"item_id": item_id})
-        item = result.fetchone()
-        if item is None:
-            raise HTTPException(status_code=404, detail="Item not found")
-    return {"name": item['name']}
+    db = SessionLocal()
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, name: str):
-    with engine.connect() as conn:
-        conn.execute(text("UPDATE items SET name = :name WHERE id = :item_id"), {"name": name, "item_id": item_id})
-        conn.commit()
-    return {"name": name}
+    db = SessionLocal()
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    item.name = name
+    db.commit()
+    return item
 
 @app.delete("/items/{item_id}")
 def delete_item(item_id: int):
-    with engine.connect() as conn:
-        conn.execute(text("DELETE FROM items WHERE id = :item_id"), {"item_id": item_id})
-        conn.commit()
+    db = SessionLocal()
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    db.delete(item)
+    db.commit()
     return {"detail": "Item deleted"}
